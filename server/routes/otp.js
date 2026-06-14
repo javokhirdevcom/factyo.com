faf8b16ef1105dcb48930b34b7253a9f4bd528a1
javokhirdevcom <javokhirdev.com@gmail.com>
2026-06-14 15:01:17 +0300

    Delete server directory

--- server/routes/otp.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const otpController = require('../controllers/otp.controller')
-
-router.post('/send', (req, res, next) => otpController.sendOtp(req, res, next))
-router.post('/verify', (req, res, next) => otpController.verifyOtp(req, res, next))
-
-module.exports = router
--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const userController = require('../controllers/user.controller')
-
-router.get('/profile/:id', (req, res, next) => userController.getUserProfile(req, res, next))
-router.put('/profile', (req, res, next) => userController.updateProfile(req, res, next))
-router.post('/change-password', (req, res, next) => userController.changePassword(req, res, next))
-router.get('/products', (req, res, next) => userController.getProducts(req, res, next))
-
-module.exports = router
